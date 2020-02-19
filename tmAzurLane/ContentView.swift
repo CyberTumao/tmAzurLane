f4bd3e42d34201dc6aff56f832a4632be30cf78b
CyberTumao <1946795889@qq.com>
2020-02-19 11:56:04 +0800

    Initial Commit

---
+++ tmAzurLane/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tmAzurLane
+//
+//  Created by tumao on 2020/2/19.
+//  Copyright © 2020 tumao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
